a9f57aa3acccf4847e7c342ce274f76b3c8df63e
khirod-datascience <66660324+khirod-datascience@users.noreply.github.com>
2024-09-20 09:26:38 +0530

    update

--- static/js/main.js
+++ static/js/main.js
@@ -7,57 +7,57 @@
 !(function($) {
   "use strict";
 
-  // Nav Menu
-  $(document).on('click', '.nav-menu a, .mobile-nav a', function(e) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var hash = this.hash;
-      var target = $(hash);
-      if (target.length) {
-        e.preventDefault();
+//   // Nav Menu
+//   $(document).on('click', '.nav-menu a, .mobile-nav a', function(e) {
+//     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+//       var hash = this.hash;
+//       var target = $(hash);
+//       if (target.length) {
+//         e.preventDefault();
 
-        if ($(this).parents('.nav-menu, .mobile-nav').length) {
-          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-          $(this).closest('li').addClass('active');
-        }
+//         if ($(this).parents('.nav-menu, .mobile-nav').length) {
+//           $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+//           $(this).closest('li').addClass('active');
+//         }
 
-        if (hash == '#header') {
-          $('#header').removeClass('header-top');
-          $("section").removeClass('section-show');
-          if ($('body').hasClass('mobile-nav-active')) {
-            $('body').removeClass('mobile-nav-active');
-            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-            $('.mobile-nav-overly').fadeOut();
-          }
-          return;
-        }
+//         if (hash == '#header') {
+//           $('#header').removeClass('header-top');
+//           $("section").removeClass('section-show');
+//           if ($('body').hasClass('mobile-nav-active')) {
+//             $('body').removeClass('mobile-nav-active');
+//             $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+//             $('.mobile-nav-overly').fadeOut();
+//           }
+//           return;
+//         }
 
-        if (!$('#header').hasClass('header-top')) {
-          $('#header').addClass('header-top');
-          setTimeout(function() {
-            $("section").removeClass('section-show');
-            $(hash).addClass('section-show');
+        // if (!$('#header').hasClass('header-top')) {
+        //   $('#header').addClass('header-top');
+        //   setTimeout(function() {
+        //     $("section").removeClass('section-show');
+        //     $(hash).addClass('section-show');
 
-          }, 350);
-        } else {
-          $("section").removeClass('section-show');
-          $(hash).addClass('section-show');
-        }
+        //   }, 350);
+        // } else {
+        //   $("section").removeClass('section-show');
+        //   $(hash).addClass('section-show');
+        // }
 
-        $('html, body').animate({
-          scrollTop: 0
-        }, 350);
+        // $('html, body').animate({
+        //   scrollTop: 0
+        // }, 350);
 
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-          $('.mobile-nav-overly').fadeOut();
-        }
+  //       if ($('body').hasClass('mobile-nav-active')) {
+  //         $('body').removeClass('mobile-nav-active');
+  //         $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+  //         $('.mobile-nav-overly').fadeOut();
+  //       }
 
-        return false;
+  //       return false;
 
-      }
-    }
-  });
+  //     }
+  //   }
+  // });
 
   // Activate/show sections on load with hash links
   if (window.location.hash) {
